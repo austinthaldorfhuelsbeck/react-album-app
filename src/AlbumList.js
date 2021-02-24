355b0858fc417b3724f2c7fceaa67d5b551df2bc
Austin Thaldorf-Huelsbeck <austinthaldorfhuelsbeck@gmail.com>
2021-02-24 15:54:02 -0800

    Initialize

---
+++ src/AlbumList.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AlbumList({ user = {} }) {
+  return <p>Please click on a user name to the left</p>;
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import AlbumList from "./AlbumList";
+import UserList from "./UserList";
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState({});
+
+  // Load data from https://jsonplaceholder.typicode.com/todos?userId=3
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left column">
+        <UserList users={users} setCurrentUser={setCurrentUser} />
+      </div>
+      <div className="right column">
+        <AlbumList user={currentUser} />
+      </div>
     </div>
   );
 }
-
-export default App;
